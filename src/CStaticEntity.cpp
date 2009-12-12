d92de0bd7292a186526b41c65e3f0360ad73ad5f
czarek.zawadka <czarek.zawadka@8e29c490-c8d6-11de-b6dc-25c59a28ecba>
2009-12-12 15:05:32 +0000

    brakujące pliki

---
+++ src/CStaticEntity.cpp
@@ -0,0 +1,14 @@
+#include "CStaticEntity.hpp"
+
+/*
+CStaticEntity::~CStaticEntity()
+{
+	//super.~CEnity();
+	cout<<"destruktor: CStaticEntity"<<endl;
+}
+*/
+
+CStaticEntity::CStaticEntity(float x, float y,	float z, const string& filename):CEntity(x,y,z,filename)
+{
+
+}
