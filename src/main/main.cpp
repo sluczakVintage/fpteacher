839652c200457fdec75326872d01ebe68c2e3f74
fester3000 <fester3000@8e29c490-c8d6-11de-b6dc-25c59a28ecba>
2010-01-11 23:42:56 +0000

    ostatni wyciek pokonany!

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,11 +1,6 @@
 #if defined(_WIN32)
 #include <vld.h>
 #endif
-/*#define _CRTDBG_MAP_ALLOC
-#define _CRTDBG_MAPALLOC 
-#include <stdlib.h>
-#include <crtdbg.h>
-*/
 
 #include <iostream>
 #include "CEngine.hpp"
@@ -17,11 +12,11 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	string s;
-	cout<<"czy uruchomic rozgrywke sieciowa? T/N\n>";
+	cout<<"Czy uruchomic rozgrywke sieciowa? T/N\n>";
 	cin>>s;
 	if(s=="T" || s == "t")
 	{
-		cout<<"podaj adres sieciowy IPv4 komputera z ktorym chcesz sie polaczyc lub wpisz 0 dla localhost\n>";
+		cout<<"Podaj adres sieciowy IPv4 komputera z ktorym chcesz sie polaczyc lub wpisz 0 dla localhost\n>";
 		cin>>s;
 
 		if(s=="0")
@@ -35,7 +30,9 @@ int main(int argc, char *argv[])
 	CEngine::getInstance()->start(); 
 //	CAuditorium::getInstance()->saveToXml();
 	CEngine::getInstance()->end();
+
 	CEngine::destroyInstance();
+	CNetwork::destroyInstance();
 	CTimer::destroyInstance();
 	return 0;
 }
