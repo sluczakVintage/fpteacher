a1e5557c817b95a860445830b674b93d21bfcb2e
fester3000 <fester3000@8e29c490-c8d6-11de-b6dc-25c59a28ecba>
2010-02-20 19:01:22 +0000

    Klasa służąca do obsługi czcionek teksturowych.

---
+++ CFont.cpp
@@ -0,0 +1,12 @@
+/** @file CFont.cpp
+* @author Sebastian £uczak
+* @date 2010.02.20
+* @version 0.2
+* @brief klasa CFont
+*	
+*		
+*/
+
+#include "CFont.hpp"
+
+//~~CFont.hpp
---
+++ CFont.hpp
@@ -0,0 +1,18 @@
+/** @file CFont.hpp
+* @author Sebastian £uczak
+* @date 2010.02.20
+* @version 0.2
+* @brief klasa CFont
+*	
+*		
+*/
+
+#ifndef	CFONT_H
+#define CFONT_H
+
+
+
+#endif
+
+//~~CFont.hpp
+
